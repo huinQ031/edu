e918e078d7fb6ae6c0876f840e3227dfa1ba8b94
huinQ031 <qc857022934984@163.com>
2021-05-23 10:04:06 +0800

    Initial commit

--- src/main/java/com/ztd/education/app/controller/UserLogin.java
+++ src/main/java/com/ztd/education/app/controller/UserLogin.java
@@ -2,13 +2,11 @@ package com.ztd.education.app.controller;
 
 import org.springframework.stereotype.Controller;
 
-/**
- *Created by OldKe on 2017/8/4.
- */
+
 @Controller
 public class UserLogin {
     public Object ok(){
-        return null;
+        return "哈哈哈";
 
     }
 }
