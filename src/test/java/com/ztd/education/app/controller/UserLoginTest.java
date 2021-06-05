7bec77d80cadd6a0b603c7ef1fb0f57081f5c527
huinQ031 <qc857022934984@163.com>
2021-06-05 18:11:25 +0800

    Initial commit

---
+++ src/test/java/com/ztd/education/app/controller/UserLoginTest.java
@@ -0,0 +1,16 @@
+package com.ztd.education.app.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by OldKe on 2017/8/4.
+ */
+public class UserLoginTest {
+    @Test
+    public void ok() throws Exception {
+
+    }
+
+}
